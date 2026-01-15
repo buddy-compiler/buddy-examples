f1a6c032d0c0466dd0bf9ae28795a7de8e7cbf64
shirohasuki <whmio0115@gmail.com>
2026-01-15 10:43:22 +0800

    [sims] fix: remove hard-coded paths in firesim config.yaml

--- sims/firesim/build-firesim.sh
+++ sims/firesim/build-firesim.sh
@@ -7,7 +7,7 @@ FIRESIM_DIR=$ROOT/sims/firesim
 
 source $ROOT/env.sh
 firesim buildbitstream \
--a $FIRESIM_DIR/yaml/config_hwdb.yaml \
--b $FIRESIM_DIR/yaml/config_build.yaml \
--r $FIRESIM_DIR/yaml/config_build_recipes.yaml \
--c $FIRESIM_DIR/yaml/config_runtime.yaml
+    -a $FIRESIM_DIR/yaml/config_hwdb.yaml \
+    -b $FIRESIM_DIR/yaml/config_build.yaml \
+    -r $FIRESIM_DIR/yaml/config_build_recipes.yaml \
+    -c $FIRESIM_DIR/yaml/config_runtime.yaml
